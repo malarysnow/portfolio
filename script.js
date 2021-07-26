0015942fe3e229da9b1027b37dea6485890c343a
malarysnow <82908635+malarysnow@users.noreply.github.com>
2021-07-26 17:51:08 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//Reveal On Scroll
+ScrollReveal().reveal(".projects", { delay: 250 });
+ScrollReveal().reveal(".education", { delay: 250 });
+ScrollReveal().reveal(".footer-container", { delay: 250 });
